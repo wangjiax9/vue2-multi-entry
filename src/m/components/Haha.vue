cac94e47b4a615e91a42d554da9d4338939df093
wangjiax9 <6215048wjl@163.com>
2017-04-24 21:00:43 +0800

    多入口分别打包

---
+++ src/m/components/Haha.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="haha">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'haha',
+  data () {
+    return {
+      msg: 'hahahahahahahahahah'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color:aquamarine;
+}
+
+</style>
